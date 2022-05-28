900bf1a1188ecffc7480256d3be1c1fd8b3b0450
Zuxxypml <priscillasweetz952@gmail.com>
2022-05-28 16:32:07 +0100

    initial commit

---
+++ src/token_assets/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Faucet from "./Faucet";
+import Balance from "./Balance";
+import Transfer from "./Transfer";
+
+function App(props) {
+  return (
+    <div id="screen">
+      <Header />
+      <Faucet userPrincipal={props.loggedInPrincipal} />
+      <Balance />
+      <Transfer />
+    </div>
+  );
+}
+
+export default App;
